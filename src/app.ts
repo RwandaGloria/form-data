5849e28f8ea7ac9abca7b10c8ff0deacc4855a25
Gloria <rwandagloria@gmail.com>
2024-12-04 21:17:52 +0100

    added app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors'; // Import cors
 import connectDB from './config/db';
 import userRouter from './routes/userRoutes';
 
 dotenv.config();
 
 const app = express();
+
+app.use(cors()); 
 
 app.use(express.json());
 
